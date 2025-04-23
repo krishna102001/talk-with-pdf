b24ecaa44515013cba1ad4b5369928186edb0d83
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-24 00:22:39 +0530

    redis setup made:

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
-
+import multer from "multer";
+import cors from "cors";
+import { handleUploadfile, startClientServer } from "./routes/route";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello world");
 });
+
+const storage = multer.diskStorage({});
+
+const upload = multer({ storage });
+app.post("/upload/pdf", upload.single("file"), handleUploadfile);
+
+startClientServer();
 
 app.listen(8080);
