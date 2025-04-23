cc25ad7189e1b4ace438493abaa4776217281904
krishna102001 <krishnakantmaurya@icloud.com>
2025-04-24 00:35:02 +0530

    pdf loader implementation done

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,6 +1,23 @@
 import { createClient } from "redis";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 const client = createClient();
+
+const handlePdfLoadData = async (pdfFile: {
+  filename: string;
+  destination: string;
+  path: string;
+}) => {
+  console.log("Processing started");
+  try {
+    const loader = new PDFLoader(pdfFile.path);
+    const docs = await loader.load();
+    console.log(docs[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export async function GetData() {
   try {
     await client.connect();
@@ -10,6 +27,7 @@ export async function GetData() {
       const fileData = await client.brPop("files", 0);
       const data = fileData?.element;
       const pdfFile = JSON.parse(data || "");
+      handlePdfLoadData(pdfFile);
       console.log(pdfFile);
     }
   } catch (error) {
